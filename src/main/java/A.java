8958bdc48f0d29c6936ced9a14d870aff6ed1b7e
liy722 <e1300504@u.nus.edu>
2025-01-24 15:44:41 +0800

    Level-2: Add, List

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class A {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        String[] list = new String[100];
 
         System.out.println("Hello! I'm A");
         System.out.println("What can I do for you?\n");
@@ -15,7 +17,16 @@ public class A {
                 break;
             }
 
-            System.out.println(input);
+            if (input.equals("list")) {
+                for (int i = 0; i < count; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                continue;
+            }
+
+            System.out.println("added: " + input);
+            list[count] = input;
+            count++;
         }
 
         scanner.close();
