60ee131846bebf6ce7d7cd7c22b5132deb7cb323
liy722 <e1300504@u.nus.edu>
2025-01-24 15:38:06 +0800

    Level-0: Rename

---
+++ src/main/java/A.java
@@ -0,0 +1,7 @@
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm A");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
