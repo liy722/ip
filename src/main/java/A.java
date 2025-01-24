cf9b877f43b18028f98b4d13d66e508bae188f9e
liy722 <e1300504@u.nus.edu>
2025-01-24 15:41:05 +0800

    Level-1: Echo

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class A {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm A");
         System.out.println("What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        scanner.close();
     }
 }
